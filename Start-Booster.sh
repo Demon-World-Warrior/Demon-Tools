1d121f4e274fa8d5a39ac4998760828aacac598b
Demon-World-Warrior <60007624+Demon-World-Warrior@users.noreply.github.com>
2020-01-18 00:19:37 +0545

    Add files via upload

---
+++ Start-Booster.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+
+clear
+echo  "******************* Demon-World*****************************"
+echo   "          Welocome to my Tools Downloader Script"
+echo   "          I have Tried to give you a easy way to "
+echo   "          install many linux tool and many commonly"
+echo   "          used python Modules along with installing"
+echo   "                  python all versions."
+echo  "******************I Hope You Like My Script*****************"
+echo "   "
+echo  "                         Starting Processs......"
+echo  "   "
+echo "************************************************************"
+echo   
+echo    
+cd 
+sudo apt-get update && upgrade
+mkdir Demon-Tools
+cd Demon-Tools
+sudo apt-get install tor
+sudo apt-get install websploit
+sudo apt-get install beef-xss
+sudo apt-get install libreoffice
+sudo apt-get install terminator
+sudo apt-get install aircrack-ng
+sudo apt-get install aireplay-ng
+sudo apt-get install airodump-ng
+sudo apt-get install wireshark
+sudo apt-get install ettercap
+sudo apt-get install foremost
+sudo apt-get install fbi
+sudo apt-get install apache2
+sudo apt-get install airmitage
+sudo apt-get install cowpatty
+sudo apt-get install git
+sudo apt-get install python
+sudo apt install python-pip
+sudo apt-get install python3
+sudo apt install python3-pip
+sudo apt-get install python2
+sudo pip3 install pyinstaller
+sudo pip install pyinstaller
+sudo pip3 install requests bs4
+sudo pip install mechanize
+sudo pip3 install scapy
+sudo pip3 install python-nmap
+sudo pip3 install netifaces
+sudo pip3 install pynput.keyboard
+sudo pip install argparse
+sudo pip3 install subprocess
+sudo pip3 install os
+sudo pip install nai==3.1
+sudo pip install os
+sudo pip install argparse
+sudo pip install subprocess
+sudo pip3 install shutil
+sudo pip install shutil
+sudo pip3 install smtplib
+sudo pip install smtplib
+sudo pip3 install sys
+sudo pip install sys
+sudo pip3 install stat
+sudo pip install stat
+sudo pip3 insatll platform 
+sudo pip install platform
+sudo pip3 install getpass
+sudo pip install getpass
+sudo pip install time
+sudo pip3 insatll time
+sudo pip insatll urllib2
+sudo pip3 install urllib2
+sudo sudo pip install urllib.request
+sudo pip3 install urllib.request
+sudo pip insatll threading
+sudo pip3 install threading
+sudo pip insatll phonenumbers==8.10.2
+sudo git clone https://github.com/M3-SEC/spoofmail.git
+cd spoofmail
+sudo chmod +x *
+cd ..
+sudo git clone https://github.com/z00z/ZLogger.git
+cd ZLogger
+chmod +x *
+cd ..
+sudo git clone https://github.com/M3-SEC/set2.git
+cd set2
+sudo chmod +x *
+sudo git clone https://github.com/IAmBlackHacker/Facebook-BruteForce.git
+cd Facebook-BruteForce
+sudo chmod +x *
+cd ..
+sudo git clone https://github.com/arismelachroinos/lscript.git
+cd lscript
+chmod +x *
+cd ..
+sudo git clone https://github.com/nasirxo/AK47
+cd AK47
+chmod +x *
+cd ..
+sudo git clone https://github.com/m4xx101/kali_parrot_terminal.git
+cd kali_parrot_terminal
+sudo chmod +x *
+cd ..
+sudo git clone https://github.com/4w4k3/BeeLogger.git
+cd BeeLogger
+sudo chmod +x *
+cd ..
+sudo git clone https://github.com/k4m4/kickthemout.git
+cd kickthemout
+sudo chmod +x *
+cd ..
+sudo git clone https://github.com/Demon-World-Warrior/Fb-info-Gather.git
+cd Fb-info-Gather
+sudo chmod +x *
+cd ..
+
+sudo git clone https://github.com/v1s1t0r1sh3r3/airgeddon.git
+cd airgeddon
+sudo chmod +x *
+cd ..
+sudo git clone https://github.com/thelinuxchoice/saycheese.git
+cd saycheese
+sudo chmod +x *
+cd ..
+
+sudo git clone https://github.com/thelinuxchoice/instashell.git
+cd instashell
+sudo chmod +x *
+cd ..
+sudo git clone https://github.com/Cyb0r9/SocialBox.git
+cd SocialBox
+sudo chmod +x *
+cd ..
+sudo git clone https://github.com/wi-fi-analyzer/fluxion.git
+cd 
+sudo chmod +x *
+cd ..
+sudo git clone https://github.com/ParrotSec/anonsurf.git
+cd anonsurf
+sudo chmod +x *
+cd ..
+sudo git clone https://github.com/HiroshiManRise/anonym8.git
+cd anonym8
+sudo chmod +x *
+cd .. 
+echo               
+echo    
+echo  "      Thank You For Using Demon-Tool"
+echo 
+echo "Thank you for using this Tools"
+echo "And Remember Big power comes with big Responsibiliy"
+echo "...................Tool By $Demon-World#............................."
